e37ae5e645464d7c2e64564809845b73166a4750
hughwang10 <hughwang10@gmail.com>
2018-01-04 17:17:47 +0000

    update

--- app/call.sh
+++ app/call.sh
@@ -14,12 +14,12 @@ http://$ts_access/simservs.ngn.etsi.org/users/sip:$ue@$realm/simservs.xml/~~/sim
 ue='+31611808148'
 ts_access='10.163.45.4'
 realm='ims.mnc001.mcc272.3gppnetwork.org'
-curl -v \
+curl -vvvvv -k \
 -H "User-Agent: 3gpp-gba" \
 -H "Accept-Charset: *" \
--H 'X-3GPP-Asserted-Identity: "sip:$ue@$realm"' \
--H 'X-XCAP-Asserted-Identity: "sip:$ue@$realm"' \
--H 'Host: xcap.ims.mnc001.mcc272.pub.3gppnetwork.org' \
-http://$ts_access:8080/mtasxdms/simservs.ngn.etsi.org/users/sip:$ue@$realm/simservs.xml/~~/simservs/communication-diversion
+-H "X-3GPP-Asserted-Identity: sip:$ue@$realm" \
+-H "X-XCAP-Asserted-Identity: sip:$ue@$realm" \
+-H "Host: xcap.ims.mnc001.mcc272.pub.3gppnetwork.org" \
+https://$ts_access:8443/mtasxdms/simservs.ngn.etsi.org/users/sip:$ue@$realm/simservs.xml/~~/simservs/communication-diversion
 # end
 
